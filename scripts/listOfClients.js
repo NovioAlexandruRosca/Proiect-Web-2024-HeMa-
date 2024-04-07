8f850bb33f1bf4312defde0dbfdf39fd8fa5519a
Rosca Alexandru-David <113398639+AlexandruRoscaPOO@users.noreply.github.com>
2024-04-07 21:10:13 +0300

    Added delete client button functionality (admin)

--- scripts/listOfClients.js
+++ scripts/listOfClients.js
@@ -12,11 +12,9 @@ function generateRandomEmail() {
 }
 
 function generateRandomTableRows(numRows) {
-    const table = document.getElementById('clientsTable');
+    const table = document.getElementById('clientsTableBody');
 
-    console.log(table);
-
-    for (let i = 0; i < numRows; i++) {
+    for (let i = 1; i < numRows; i++) {
         const id = i + 1;
         const name = generateRandomString(10) + ' ' + generateRandomString(10);
         const email = generateRandomEmail();
@@ -25,11 +23,19 @@ function generateRandomTableRows(numRows) {
             <th><h2 class="idcol">${id}</h2></th>
             <th class="scrollable-content"><h2>${name}</h2></th>
             <th><h2>${email}</h2></th>
-            <th><h2 class="emoji">🗑️</h2></th>
+            <th class="deleteBtn"><h2 class="emoji">🗑️</h2></th>
             <th><h2 class="emoji">🌱</h2></th>
         `;
         table.appendChild(row);
     }
 }
 
-generateRandomTableRows(50);
+generateRandomTableRows(20);
+
+const deleteButtons = document.querySelectorAll('.deleteBtn');
+deleteButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const row = button.closest('tr');
+        row.remove();
+    });
+});
