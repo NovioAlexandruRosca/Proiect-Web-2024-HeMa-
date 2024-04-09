dfea63c4e26592dd75eb04c09f6be27becdbbf57
Rosca Alexandru-David <113398639+AlexandruRoscaPOO@users.noreply.github.com>
2024-04-09 14:08:39 +0300

    Added landing page

---
+++ scripts/landingPage.js
@@ -0,0 +1,84 @@
+const initSlider = () => {
+    const container = document.getElementById('container');
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    const sliderScrollbar = document.querySelector("#container .slider-scrollbar");
+    const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+    
+    let scrollDirection = 1;
+
+    // Auto slide function
+    const autoSlide = () => {
+        const scrollAmount = imageList.clientWidth/ 500 * scrollDirection;
+        imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+
+    // Toggle scroll direction when reaching the sides of the image list
+    if (imageList.scrollLeft >= maxScrollLeft) {
+        scrollDirection = -1; // Change direction to left
+    } else if (imageList.scrollLeft <= 0) {
+        scrollDirection = 1; // Change direction to right
+    }
+    };
+
+    // Set interval for auto slide (change 3000 to desired interval in milliseconds)
+    let autoSlideInterval = setInterval(autoSlide, 50);
+
+    // Stop auto slide on mouse enter
+    container.addEventListener("mouseenter", () => {
+        clearInterval(autoSlideInterval);
+    });
+
+    // Resume auto slide on mouse leave
+    container.addEventListener("mouseleave", () => {
+        autoSlideInterval = setInterval(autoSlide, 50);
+    });
+
+    // Slide images according to the slide button clicks
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth / 3 * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+
+     // Show or hide slide buttons based on scroll position
+    const handleSlideButtons = () => {
+        slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+        slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+    }
+
+    // Update scrollbar thumb position based on image scroll
+    const updateScrollThumbPosition = () => {
+        const scrollPosition = imageList.scrollLeft;
+        const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+        scrollbarThumb.style.left = `${thumbPosition}px`;
+    }
+
+    // Call these two functions when image list scrolls
+    imageList.addEventListener("scroll", () => {
+        updateScrollThumbPosition();
+        handleSlideButtons();
+    });
+}
+
+window.addEventListener("resize", initSlider);
+window.addEventListener("load", initSlider);
+
+
+let loginClients = document.getElementById("loginClients");
+let loginAdmin = document.getElementById("loginAdmin");
+let registerClients = document.getElementById("registerClients");
+
+loginClients.addEventListener('click', function (event) {
+    window.location.href = '../html/login.html';
+});
+
+loginAdmin.addEventListener('click', function (event) {
+    window.location.href = '../html/admin.html';
+});
+
+registerClients.addEventListener('click', function (event) {
+    window.location.href = '../html/register.html';
+});
--- scripts/login.js
+++ scripts/login.js
@@ -1,7 +1,7 @@
 const register_button = document.getElementById("register");
 
 register_button.addEventListener("click",() => {
-    window.location.href = "/register.html";
+    window.location.href = "../html/register.html";
 });
 
 document.getElementById("Email").addEventListener("click", () => {
@@ -12,51 +12,51 @@ document.getElementById("Password").addEventListener("click", () => {
     document.getElementById("Password").placeholder = "";
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const loginForm = document.getElementById('login_form');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const loginForm = document.getElementById('login_form');
 
-    loginForm.addEventListener('submit', function (event) {
+//     loginForm.addEventListener('submit', function (event) {
 
-        event.preventDefault();
+//         event.preventDefault();
 
-        const username = document.getElementById('Email').value;
-        const password = document.getElementById('Password').value;
+//         const username = document.getElementById('Email').value;
+//         const password = document.getElementById('Password').value;
 
-        let auth = true;
+//         let auth = true;
 
-        if(username == "") 
-        {
-            document.getElementById("Email").placeholder = "Can't leave empty";
-            auth = false;
-        }
-        if(password == "")
-        {
-            document.getElementById("Password").placeholder = "Can't leave empty";
-            auth = false;
-        }
+//         if(username == "") 
+//         {
+//             document.getElementById("Email").placeholder = "Can't leave empty";
+//             auth = false;
+//         }
+//         if(password == "")
+//         {
+//             document.getElementById("Password").placeholder = "Can't leave empty";
+//             auth = false;
+//         }
 
-        if(auth)
-        {
-            fetch('/testCredentials', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    Email: username,
-                    Password: password,
-                }),
-            })
-            .then((response) => {
-                if (response.status === 401) {
-                    document.getElementById("Error_Message").innerText = "Invalid Credentials!";
-                }
-                else
-                    window.location.href = "/index.html";
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-        }
-    });
-});
+//         if(auth)
+//         {
+//             fetch('/testCredentials', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                 },
+//                 body: JSON.stringify({
+//                     Email: username,
+//                     Password: password,
+//                 }),
+//             })
+//             .then((response) => {
+//                 if (response.status === 401) {
+//                     document.getElementById("Error_Message").innerText = "Invalid Credentials!";
+//                 }
+//                 else
+//                     window.location.href = "/index.html";
+//             })
+//             .catch((error) => {
+//                 console.error('Error:', error);
+//             });
+//         }
+//     });
+// });
